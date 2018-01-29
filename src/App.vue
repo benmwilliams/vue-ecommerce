142c72544e67856488cbadce5107620809cc33c9
Ben Williams <benwilliamsdev@gmail.com>
2018-01-29 00:27:52 -0500

    subrouting

--- src/App.vue
+++ src/App.vue
@@ -27,7 +27,7 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: rgba(0, 0, 0, .8);
   margin-top: 60px;
 }
 
--- src/router/index.js
+++ src/router/index.js
@@ -1,28 +1,49 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-// import HelloWorld from '@/components/HelloWorld';
-import Home from '@/components/pages/Home';
-import Admin from '@/components/pages/Admin';
+
 import Cart from '@/components/pages/Cart';
+import Home from '@/components/pages/Home';
+
+import Edit from '@/components/pages/admin/Edit';
+import Index from '@/components/pages/admin/Index';
+import New from '@/components/pages/admin/New';
+import Products from '@/components/pages/admin/Products';
 
 Vue.use(Router);
 
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: 'Home',
-      component: Home,
-    },
-    {
       path: '/admin',
       name: 'Admin',
-      component: Admin,
+      component: Index,
+      children: [
+        {
+          path: 'edit/:id',
+          name: 'Edit',
+          component: Edit,
+        },
+        {
+          path: 'new',
+          name: 'New',
+          component: New,
+        },
+        {
+          path: '',
+          name: 'Products',
+          component: Products,
+        },
+      ],
     },
     {
       path: '/cart',
       name: 'Cart',
       component: Cart,
+    },
+    {
+      path: '/',
+      name: 'Home',
+      component: Home,
     },
   ],
 });
